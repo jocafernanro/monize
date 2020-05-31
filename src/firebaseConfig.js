4e7fae6f2a55493c1315185de59ccde917d2c718
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-05-31 12:11:51 +0200

    firebaseConfig uploaded

---
+++ src/firebaseConfig.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: `${process.env.VUE_APP_MONIZE_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_MONIZE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.VUE_APP_MONIZE_DATABASE_URL}`,
+  projectId: `${process.env.VUE_APP_MONIZE_PROJECT_ID}`,
+  storageBucket: `${process.env.VUE_APP_MONIZE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VUE_APP_MONIZE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.VUE_APP_MONIZE_APP_ID}`,
+  measurementId: `${process.env.VUE_APP_MONIZE_MEASUREMENT_ID}`
+}
+
+firebase.initializeApp(config)
+firebase.analytics()
+
+// firebase utils
+const db = firebase.firestore()
+const settings = {}
+db.settings(settings)
+
+// firebase colections
+const productsCollection = db.collection('products')
+const brandsCollection = db.collection('brands')
+
+export {
+  db,
+  productsCollection,
+  brandsCollection
+}
