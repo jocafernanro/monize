e1932cb3377b794d2ca30178c373319701e10c55
Josseph <jossephalvarez@gmail.com>
2020-06-02 15:26:36 +0200

    (feat): vsax button added

--- src/components/CookieLaw.vue
+++ src/components/CookieLaw.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="main-wrapper">
     <cookie-law theme="blood-orange--rounded">
-      <div slot="message">
-        This website uses cookies to ensure you get the best experience on our website. More info <router-link to="/about">Click here</router-link>
+      <div slot-scope="props">
+        <p>
+          This website uses cookies 🍪 to ensure you get the best experience on our website. More info <router-link to="/about" class="aboutLink">Click here</router-link>
+        </p>
+        <div class="center grid cookieButton">
+          <vs-button
+            color="rgb(59,222,200)"
+            gradient
+            @click="props.accept"
+          >
+            I accept
+          </vs-button>
+          <vs-button
+            danger
+            border
+            @click="props.close"
+          >
+            Ignore me
+          </vs-button>
+        </div>
       </div>
     </cookie-law>
   </div>
@@ -17,3 +35,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+  .aboutLink{
+    color:dodgerblue;
+  }
+  .cookieButton{
+    display: flex;
+  }
+</style>
