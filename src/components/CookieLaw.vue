2424d76f1fe3ef3cbfcd4762d0cb0fb7eeee6117
Josseph <jossephalvarez@gmail.com>
2020-06-02 14:05:27 +0200

    (feat): cookies router Link

--- src/components/CookieLaw.vue
+++ src/components/CookieLaw.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="main-wrapper">
-    <cookie-law theme="blood-orange--rounded"></cookie-law>
+    <cookie-law theme="blood-orange--rounded">
+      <div slot="message">
+        This website uses cookies to ensure you get the best experience on our website. More info <router-link to="/about">Click here</router-link>
+      </div>
+    </cookie-law>
   </div>
 </template>
 
