59b98daf0e4472ced2d4d7ca57748d5c4e4865f9
Josseph <jossephalvarez@gmail.com>
2020-06-05 12:47:26 +0200

    (feat): Legal View added

--- src/lang/es.js
+++ src/lang/es.js
@@ -8,6 +8,22 @@ const messages = {
       secondSubtitle: 'Si la web es gratis ¿cómo se monetiza?',
       secondParagraph: 'Queremos ser totalmente transparentes con nuestra web ya que solo queremos ofrecerte el mejor valor posible junto con la tranquilidad de conocer cómo funciona nuestra web. Para sustentar el funcionamiento de los servidores y la continua mejora de esta web, cada elnace de cada producto se genera con lo que se conoce como un enlace de afiliados. Esto es un modelo muy justo con las tres partes: cliente, tienda y web. Cuando haces click en el chollo que publicamos y compras con él, amazon no altera el precio que tenga ese producto, pero sin embargo a nosotros nos da un pequeño porcentaje de esa compra. Por lo tanto, el cliente no ve alterado el precio de la web, nosotros ganamos un procentaje por darte valor y la tienda paga de forma justa esa colaboración. RECUERDA: el precio no se ve alteado en ningún momento, así que si te gusta lo que hacemos y quieres colaborar, la mejor forma es simplemente comprar con nuestro enlace del producto, ya que nos estarás ayudando mucho con ese perqueño porcentaje de la compra.'
     },
+    legal: {
+      title: 'Términos y Condiciones de Uso',
+      firstSubtitle: 'INFORMACIÓN RELEVANTE',
+      firstParagraph: 'Es requisito necesario para la adquisición de los productos que se ofrecen en este sitio, que lea y acepte los siguientes Términos y Condiciones que a continuación se redactan. El uso de nuestros servicios así como la compra de cualquier producto o servicio en esta web implica que usted ha leído y aceptado los Términos y Condiciones de Uso en el presente documento.\n\nTodas los productos y/o servicios   que son ofrecidos por nuestro sitio web pudieran ser creadas, cobradas, enviadas o presentadas por una página web tercera y en tal caso estarían sujetas a sus propios Términos y Condiciones. Todas las compras y transacciones que se lleven a cabo por medio de este sitio web, están sujetas a un proceso de confirmación y verificación, el cual podría incluir la verificación del stock y disponibilidad de producto, validación de la forma de pago, validación de la factura (en caso de existir) y el cumplimiento de las condiciones requeridas por el medio de pago seleccionado. En algunos casos puede que se requiera una verificación por medio de correo electrónico.\n\n' +
+        'Los precios de los productos ofrecidos en esta web son válidos solamente en las compras realizadas a través este sitio web.\n',
+      secondSubtitle: 'LICENCIA',
+      secondParagraph: 'Monize  a través de su sitio web concede una licencia para que los usuarios utilicen  los productos que son vendidos en este sitio web de acuerdo a los Términos y Condiciones que se describen en este documento.',
+      thirdSubtitle: 'PROPIEDAD',
+      thirdParagraph: 'Usted no puede declarar propiedad intelectual o exclusiva a ninguno de los productos expuestos en Monize, modificado o sin modificar. Todos los productos son propiedad  de los proveedores del contenido. En caso de que no se especifique lo contrario, los productos se proporcionan  sin ningún tipo de garantía, expresa o implícita por parte de Monize. Cada proveedor del producto se encargará de la misma.',
+      fourthTitle: 'POLÍTICA DE REEMBOLSO Y GARANTÍA',
+      fourthParagraph: 'Monize no realiza ningún reembolso de productos comprados a traves de nuestra web. Le pedimos que lea cuidadosamente los condiciones del proveedor  que le servirá el producto antes de comprarlo.',
+      fifthTitle: 'PRIVACIDAD',
+      fifthParagraph: 'Monize garantiza que la información personal que usted envía cuenta con la seguridad necesaria. Los datos ingresados por usuario o en el caso de requerir una validación de los pedidos no serán entregados a terceros, salvo que deba ser revelada en cumplimiento a una orden judicial o requerimientos legales.\n' +
+        '\n\nLa suscripción a boletines de correos electrónicos publicitarios es voluntaria y podría ser seleccionada al momento de crear su cuenta.\n' +
+        '\n\nMonize reserva los derechos de cambiar o de modificar estos términos sin previo aviso.\n'
+    },
     cookies: {
       info: 'Este sitio web usa cookies 🍪 para asegurarte de que obtienes la mejor experiencia en nuestra web. Más info',
       clickHere: 'Click aquí',
---
+++ src/views/Legal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <section class="about__info">
+      <h1 class="about__info__title">{{ $t("legal.title") }}</h1>
+      <h2 class="about__info__subtitle">{{ $t("legal.firstSubtitle") }}</h2>
+      <p class="about__info__paragraph">{{ $t("legal.firstParagraph") }}</p>
+      <h2 class="about__info__subtitle">{{ $t("legal.secondSubtitle") }}</h2>
+      <p class="about__info__paragraph">{{ $t("legal.secondParagraph") }}</p>
+      <h2 class="about__info__subtitle">{{ $t("legal.thirdSubtitle") }}</h2>
+      <p class="about__info__paragraph">{{ $t("legal.thirdParagraph") }}</p>
+      <h2 class="about__info__subtitle">{{ $t("legal.fourthTitle") }}</h2>
+      <p class="about__info__paragraph">{{ $t("legal.fourthParagraph") }}</p>
+      <h2 class="about__info__subtitle">{{ $t("legal.fifthTitle") }}</h2>
+      <p class="about__info__paragraph">{{ $t("legal.fifthParagraph") }}</p>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  margin-top: 1.5em;
+}
+
+.about__info {
+  width: 80%;
+  max-width: var(--max-width);
+  display: grid;
+  min-width: 0;
+  min-height: 0;
+
+  &__title {
+    font-size: 2rem;
+  }
+
+  &__subtitle {
+    font-size: 1.5rem;
+  }
+  &__paragraph {
+    font-size: 1.5rem;
+    white-space: pre-line;
+  }
+}
+</style>
