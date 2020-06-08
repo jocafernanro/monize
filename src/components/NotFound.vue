8cd67c511422ce472c3c494fbb9cb1d4af199568
Josseph <jossephalvarez@gmail.com>
2020-06-08 10:56:53 +0200

    (feat): not found styles v1

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="notFound center">
+    <img class="notFound__img" src="../assets/images/404.png">
     <section class="notFound__info">
       <h1 class="notFound__info__title">
         {{ $t("notFound.title") }}
       </h1>
-      <router-link to="/">
-        <vs-button
+      <vs-button
+          size="xl"
           border
+          to="/"
+          flat
         >
           Volver a la principal
-        </vs-button>
-      </router-link>
+      </vs-button>
     </section>
   </div>
 
@@ -23,15 +25,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .notFound {
+/*  .notFound {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
     margin-top: 1.5em;
     &__info, &__button{
       display: grid;
     }
+  }*/
 
+  .notFound{
+    position: relative;
+    text-align: center;
+
+    &__info{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      &__title{
+        font-size: 2rem;
+        margin-top: 8rem;
+      }
+    }
   }
 
 </style>
